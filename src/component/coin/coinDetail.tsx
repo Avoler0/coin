c75f6b2626deba2fded87550b6582e1bcc602d0f
Avoler0 <jys1391@gmail.com>
2023-04-09 21:59:41 +0900

    Feat: 코인 head scroll 감지 fixed 추가

--- src/component/coin/coinDetail.tsx
+++ src/component/coin/coinDetail.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import './coinDetail.css'
-import React from 'react'
+import React, { useCallback } from 'react'
 import CoinGraph from './coinGraph';
 import upbitApi from '../../hook/upbit-api';
 import * as d3 from "d3";
@@ -55,6 +55,7 @@ function CoinDetail(){
   const tablet = useMediaQuery("(min-width:521px) and (max-width: 1025px)");
   const mobile = useMediaQuery("(max-width:520px)");
   const user = useSelector((state:any)=> state.user )
+  const [scroll,setScroll] = React.useState(false);
   const [chartData,setChartData] = React.useState<any>(null);
   const [overData,setOverData] = React.useState<any>(null);
   const market = useSelector((state:any) => state.market)
@@ -62,6 +63,16 @@ function CoinDetail(){
   const navigate = useNavigate();
   const location = useLocation();
 
+
+  const handleScroll = useCallback((e:any) => {
+    const scrollY = window.scrollY;
+    
+    if(scrollY <= 100){
+      setScroll(false)
+    }else{
+      setScroll(true)
+    }
+  }, [])
   React.useEffect(()=>{
     if(!user.loginState){
       alert('로그인 후 이용해주세요.')
@@ -73,6 +84,11 @@ function CoinDetail(){
       setChartData(_res)
       setOverData(_res[0].data[0])
     })
+
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
   },[location])
   
   if(!chartData) return <div></div>
@@ -90,54 +106,57 @@ function CoinDetail(){
   
   return(
     <div className='coin-detail'>
-      <div className='head'>
-        <div>
-          <h2 className='name'>
-            {location.search.split('=')[1]}
-          </h2>
+      <div className='head-wrap'>
+        <div className={`head ${scroll ? 'scroll' : null}`}>
+          <div>
+            <h2 className='name'>
+              {location.search.split('=')[1]}
+            </h2>
+            <form className='search-form'
+              onSubmit={searchSubmit}
+            >
+              <InputTextField
+                label="코인 검색"
+                variant="standard"
+                size="small"
+                margin='normal'
+                sx={{
+                  ' .MuiOutlinedInput-root': {
+                      color: 'white',
+                        border: '1px solid red',
+                    },
+                }}
+                onChange={(event:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setSearchValue(event.target.value)}
+              />
+        </form>
+          </div>
+          <div className='info'>
+            <div className='left'>
+              <span>시가</span>
+              <span className='number'>{overData ? overData?.opening_price.toLocaleString('ko-KR') : ''}</span>
+            </div>
+            <div className={`left ${overData && isNegative(overData.change_price) ? 'minus' : 'plus'}`}>
+              <span>고가</span>
+              <span className='number'>{overData ?  overData?.high_price.toLocaleString('ko-KR') : ''}</span>
+            </div>
+            <div className={`right ${overData && isNegative(overData.change_price) ? 'minus' : 'plus'}`}>
+              <span>저가</span>
+              <span className='number'>{overData ? overData.low_price.toLocaleString('ko-KR') : ''}</span>
+            </div>
+            <div className={`right ${overData && isNegative(overData.change_price) ? 'minus' : 'plus'}`}>
+              <span>종가</span>
+              <span className='number'>{overData ? overData.trade_price.toLocaleString('ko-KR') : ''}</span>
+            </div>
+          </div>
         </div>
-        <div className='info'>
-          <div className='left'>
-            <span>시가</span>
-            <span className='number'>{overData ? overData?.opening_price.toLocaleString('ko-KR') : ''}</span>
-          </div>
-          <div className={`left ${overData && isNegative(overData.change_price) ? 'minus' : 'plus'}`}>
-            <span>고가</span>
-            <span className='number'>{overData ?  overData?.high_price.toLocaleString('ko-KR') : ''}</span>
-          </div>
-          <div className={`right ${overData && isNegative(overData.change_price) ? 'minus' : 'plus'}`}>
-            <span>저가</span>
-            <span className='number'>{overData ? overData.low_price.toLocaleString('ko-KR') : ''}</span>
-          </div>
-          <div className={`right ${overData && isNegative(overData.change_price) ? 'minus' : 'plus'}`}>
-            <span>종가</span>
-            <span className='number'>{overData ? overData.trade_price.toLocaleString('ko-KR') : ''}</span>
-          </div>
-        </div>
+        
       </div>
-      <form className='search-form'
-        onSubmit={searchSubmit}
-      >
-        <InputTextField
-          label="코인 검색"
-          variant="standard"
-          size="small"
-          margin='normal'
-          sx={{
-            ' .MuiOutlinedInput-root': {
-                color: 'white',
-                  border: '1px solid red',
-              },
-          }}
-          onChange={(event:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setSearchValue(event.target.value)}
-        />
-      </form>
       <Grid container className='graph-wrap' 
         justifyContent="space-between"
         >
             {chartData && chartData.map((data:any,i:number) => {
               return (
-                <GridItem item style={{height:600}} key={i} xs={tablet ? 6 : mobile ? 12 : 3}>
+                <GridItem item style={{height:500}} key={i} xs={tablet ? 6 : mobile ? 12 : 3}>
                   <CoinGraph chartData={data.data} setOverData={setOverData} />
                   
                 </GridItem>
